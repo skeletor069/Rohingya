72794fdc3402b80114107bfaa10373699e116f78
TJ <skeletor069@gmail.com>
2018-11-14 18:26:01 +0100

    pedestrian included

---
+++ Assets/Scripts/_slum_scene/PublicPlaces.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PublicPlaces : MonoBehaviour {
+	private static List<Vector3> publicPlaces;
+
+	void Awake () {
+		publicPlaces = new List<Vector3>();
+		for(int i = 0 ; i < transform.childCount; i++)
+			publicPlaces.Add(transform.GetChild(i).position);
+	}
+
+	public static Vector3 GetRandomPosition() {
+		int index = Random.Range(0, publicPlaces.Count);
+		return publicPlaces[index];
+	}
+}
---
+++ Assets/Scripts/_slum_scene/_facilities/Dhaba.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dhaba : Facility {
+	public override void InitiateData() {
+		facilityName = "Dhaba";
+		facilityDescription = "Some guys selling low quality foods for lunch and dinner";
+		optionNames[0] = "small lunch plate (-৳35)";
+		optionNames[1] = "big lunch plate (-৳60)";
+		optionNames[2] = "bread with curry (-৳30)";
+	}
+
+	public override void Action1() {
+		List<AttributeToken> tokens = new List<AttributeToken>();
+		tokens.Add(new AttributeToken(HeroAttributes.FOOD, 60));
+		tokens.Add(new AttributeToken(HeroAttributes.MONEY, -35));
+		GameController.GetInstance().World.ActionPerformed(tokens, 15);
+	}
+
+	public override void Action2() {
+		List<AttributeToken> tokens = new List<AttributeToken>();
+		tokens.Add(new AttributeToken(HeroAttributes.FOOD, 100));
+		tokens.Add(new AttributeToken(HeroAttributes.MONEY, -60));
+		GameController.GetInstance().World.ActionPerformed(tokens, 20);
+	}
+
+	public override void Action3() {
+		List<AttributeToken> tokens = new List<AttributeToken>();
+		tokens.Add(new AttributeToken(HeroAttributes.FOOD, 50));
+		tokens.Add(new AttributeToken(HeroAttributes.MONEY, -30));
+		GameController.GetInstance().World.ActionPerformed(tokens, 15);
+	}
+
+	public override void DoJob() {
+		List<AttributeToken> tokens = new List<AttributeToken>();
+		tokens.Add(new AttributeToken(HeroAttributes.MONEY, 5));
+		tokens.Add(new AttributeToken(HeroAttributes.ENERGY, -35));
+		GameController.GetInstance().World.ActionPerformed(tokens, 60);
+	}
+}
